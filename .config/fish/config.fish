dae3a77f232c4a1e6e45afc36500857387f9dda5
Massimo Ruohonen <massimo.ruohonen@outlook.com>
2024-09-07 20:51:14 +0300

    Update nano bindings and set PATH
    Cherry-pick from zen

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,16 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    set fish_term24bit 1
+    
     bind \b backward-kill-word
     bind \e\[3\;5~ kill-word
     bind  -k nul accept-autosuggestion
 
     abbr -a sctl systemctl
     abbr -a --set-cursor sysd systemd%
+
+end
+
+if status is-login
+    fish_add_path ~/.local/bin
 end
